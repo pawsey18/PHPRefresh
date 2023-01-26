a6aab0bcb33977ff7738c68b5546e4bff0f36619
Nick <pawsey18@gmail.com>
2023-01-26 13:07:49 -0400

    Clean up

--- index.php
+++ index.php
@@ -3,41 +3,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="<link rel=" stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-
+    <title>PHP Refresher</title>
 </head>
 
 <body>
-    <?php
-
-    // $fizz = 5;
-    // $buzz = 3;
-
-    // $count = 100;
-
-    // for ($i = 1; $i <= $count; $i++) {
-    //     if (($i % $buzz) && ($i % $fizz) == 0) {
-    //         echo "<p>" . "fizzbuzz |" . $i . "</p>";
-    //     } else if ($i % $fizz == 0) {
-    //         echo "<p>" . "fizz |" . $i . "</p>";
-    //     } else if ($i % $buzz == 0) {
-    //         echo "<p>" . "buzz |" . $i . "</p>";
-    //     } else {
-    //         echo "<p>$i</p>";
-    //     }
-    // }
-    //            echo phpinfo();
-    // $var = 678;
-    // if ("hell"){
-    //     jfsdkfj 
-    // }
-    ?>
-
     <?php
     $sql = "select * from authors";
     $result = $conn->query($sql);
@@ -56,7 +25,7 @@
     if (mysqli_close($conn)) {
         echo "connection closed";
     }
-    // }    
+
     $str = 'áéóú';
     echo mb_detect_encoding($str);
 
